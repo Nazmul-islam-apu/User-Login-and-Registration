3031d0197ae38145e23286dfe66efd21ece97a68
Nazmul Islam <nazmul.rhinewaal@gmail.com>
2021-05-02 21:47:01 +0200

    update

---
+++ src/main/java/com/example/UserRegistration/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.UserRegistration.repository;
+
+import com.example.UserRegistration.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
